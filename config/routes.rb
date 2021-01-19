65ce1b45f9556b5fd5ffdb80c838bb7868ea5ee2
LaNice Powell <lanicemp@aol.com>
2021-01-19 13:23:10 -0500

    updated namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
  
+  
   namespace :api do 
     namespace :v1 do 
-     resources :users 
-     resources :meetings
+     resources :users,only: [:index, :create, :destroy, :update] 
+     resources :meetings, only: [:index, :create, :destroy, :update]
+     resources :my_meetings, only: [:index, :create, :destroy, :update]
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
