11eea5f4f94dc141028366ed1d7f17a9a4d17121
LaNice Powell <lanicemp@aol.com>
2021-01-20 02:01:47 -0500

    removed helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::API
-helpers do 
-    def current_user
-        User.first
-    end 
-end 
+  
+        def current_user
+            User.first
+        end 
+    
+
+        def logged_in
+            !!current_user
+        end
+    
+    
 
 end
