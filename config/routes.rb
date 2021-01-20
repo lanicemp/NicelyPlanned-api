9d251f2d2d8b6b2013aa30bda8693f35a55e5d86
LaNice Powell <lanicemp@aol.com>
2021-01-19 21:52:32 -0500

    added show to the resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   
   namespace :api do 
     namespace :v1 do 
-     resources :users,only: [:index, :create, :destroy, :update] 
-     resources :meetings, only: [:index, :create, :destroy, :update]
-     resources :my_meetings, only: [:index, :create, :destroy, :update]
+     resources :users,only: [:index, :show, :create, :destroy, :update] 
+     resources :meetings, only: [:index, :show, :create, :destroy, :update]
+     resources :my_meetings, only: [:index, :show, :create, :destroy, :update]
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
