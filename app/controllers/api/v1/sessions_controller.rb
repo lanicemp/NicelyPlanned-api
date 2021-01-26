034addf2da03f9cf299f180a7a8bbb69b252f8da
LaNice Powell <lanicemp@aol.com>
2021-01-26 00:47:27 -0500

    updated create and get current user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,26 @@
 class Api::V1::SessionsController < ApplicationController
 
     def create 
-        byebug
+        @user = User.find_by(email: params[:session][:email])
+
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            render json: @user
+        else 
+            render json: {
+                error: "Invalid Credentials"
+            }
+        end
     end 
 
-    
-    def 
+    def get_current_user 
+        if logged_in?
+            render json: current_user
+        else 
+            render json: {
+                notice: "No one logged in"
+            }
+        end
     end
+    
 end
