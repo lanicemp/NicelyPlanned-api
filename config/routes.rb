c11d9417c97177a2eef8effb880fb0245cb1c715
LaNice Powell <lanicemp@aol.com>
2021-01-26 00:54:20 -0500

    added namespacing for get_current_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
- post "/api/vl/login", to: "api/v1/sessions#create" 
+ post "/api/v1/login", to: "api/v1/sessions#create" 
+ get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   
   namespace :api do 
     namespace :v1 do 
-     resources :users do
-     resources :meetings, only: [:index, :show, :create, :destroy, :update]
-     resources :my_meetings, only: [:index, :show, :create, :destroy, :update]
+     resources :users 
+     resources :meetings
+     resources :my_meetings
     end 
   end 
   end 
