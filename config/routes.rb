821f68f72f476bdc8a6028086f2911ae52b2f16f
LaNice Powell <lanicemp@aol.com>
2021-01-25 22:46:21 -0500

    namespaced

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
- get "api/vl/login", to: "sessions#create" 
+ post "/api/vl/login", to: "api/v1/sessions#create" 
   
   namespace :api do 
     namespace :v1 do 
-     resources :users,only: [:index, :show, :create, :destroy, :update] 
+     resources :users do
      resources :meetings, only: [:index, :show, :create, :destroy, :update]
      resources :my_meetings, only: [:index, :show, :create, :destroy, :update]
     end 
+  end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
