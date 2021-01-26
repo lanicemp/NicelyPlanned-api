569d07ead48bc7dafadfa41fc719b25c40b49d0b
LaNice Powell <lanicemp@aol.com>
2021-01-26 14:13:23 -0500

    removed end

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
      resources :users 
      resources :meetings
      resources :my_meetings
-    end 
+    
   end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
