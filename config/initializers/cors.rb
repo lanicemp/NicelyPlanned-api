60c07a8a84c47f7172ded7601e1a9ee472e66e77
LaNice Powell <lanicemp@aol.com>
2021-01-26 14:13:51 -0500

    removed local host 3000

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000', "http://localhost:3000"
+    origins 'http://localhost:4000'
+    # connecting the frontend to the backend by the webaddress
     # resource '/cors',
     # :headers => :any,
     # :methods => [:post],
