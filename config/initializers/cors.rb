205eaffebf40bdbde538af5de66e4565ae18fff2
LaNice Powell <lanicemp@aol.com>
2021-01-25 22:46:53 -0500

    commented out code

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,13 +8,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/cors',
-    :headers => :any,
-    :methods => [:post],
-    :max_age => 0
+    # resource '/cors',
+    # :headers => :any,
+    # :methods => [:post],
+    # :max_age => 0
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      
+
   end
 end
