6613d5ac4992c1375d6040da253195291ba3b962
LaNice Powell <lanicemp@aol.com>
2021-01-27 16:44:06 -0500

    updated link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000'
+    origins 'http://localhost:4000/'
     # connecting the frontend to the backend by the webaddress
     # resource '/cors',
     # :headers => :any,
