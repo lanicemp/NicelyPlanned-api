fcfd812a7fc86325e9f90533de5840f7ec612a97
LaNice Powell <lanicemp@aol.com>
2021-01-31 11:17:01 -0500

    updated meeting attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_181310) do
+ActiveRecord::Schema.define(version: 2021_01_31_154415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "meetings", force: :cascade do |t|
-    t.string "name"
     t.date "date"
-    t.time "time"
     t.string "detail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.time "end_time"
     t.string "comment"
     t.integer "user_id"
+    t.time "start_time"
+    t.string "title"
   end
 
   create_table "my_meetings", force: :cascade do |t|
