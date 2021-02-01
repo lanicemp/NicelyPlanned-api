bcabc85f6c622c0deccb61a9adde31c4d2e477aa
LaNice Powell <lanicemp@aol.com>
2021-02-01 16:42:00 -0500

    added email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
     validates :name, :email, presence:true 
     validates :email, uniqueness: true
 
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+
 end
