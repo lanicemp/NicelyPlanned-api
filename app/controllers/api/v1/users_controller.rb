6a68c9362250e61cff2a24c09e4733a3946bb0df
LaNice Powell <lanicemp@aol.com>
2021-02-01 16:42:52 -0500

    changed password digest to password

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -54,6 +54,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest)
+      params.require(:user).permit(:name, :email, :password)
     end
 end
