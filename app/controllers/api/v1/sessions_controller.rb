5265fe4ac4eb945c0e5bb1eb11bb6d3d88636921
LaNice Powell <lanicemp@aol.com>
2021-02-01 13:00:20 -0500

    removed spacing

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -16,10 +16,8 @@ class Api::V1::SessionsController < ApplicationController
 
     def get_current_user 
         if logged_in
-       
             render json: UserSerializer.new(current_user)
-            
-          
+                    
         else 
             render json: {
                 error: "No one logged in"
