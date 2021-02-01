f3960d39cf45dc7ef37b415edbc490b920067044
LaNice Powell <lanicemp@aol.com>
2021-02-01 12:57:47 -0500

    adde UserSerializer to create method.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -21,7 +21,8 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
       # , location: @user
     else
       resp = {
